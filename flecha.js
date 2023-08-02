71a0bbaf01a607e686c4974fbb491df385578493
Gonzalo <gonzalofrascheri@gmail.com>
2023-08-02 15:12:55 -0300

    first commit

---
+++ flecha.js
@@ -0,0 +1,13 @@
+
+
+//function sumar (a, b){
+
+//    return a + b;
+//}
+
+const sumar = (a,b) => a + b;
+
+const saludar = () => 'Hola Mundo';
+
+console.log(saludar());
+console.log(sumar(5,10));
